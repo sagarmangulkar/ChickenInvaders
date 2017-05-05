bc599c190411ac731bf422894e45e6f60ab2dd1e
Sagar Mangulkar <smangul1@binghamton.edu>
2017-05-04 21:16:11 -0400

    Implement up and down motion of chicken

--- PlayViewController.swift
+++ PlayViewController.swift
@@ -17,11 +17,14 @@ class PlayViewController: UIViewController {
     @IBOutlet var buttonRight: UIButton!
     @IBOutlet var imageHero: UIImageView!
     let motionDistance = 50         //motion distance when push navigation panel button
+    var xChickenMotion = 0
+    var yChickenMotion = 0
+    var isUpward = false
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-            timers()
+        timers()
         //moveChicken()
         //moveChickenInCurveMotion()
         
@@ -34,7 +37,8 @@ class PlayViewController: UIViewController {
     }
     
     func timers(){
-        var timerMoveChicken = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(moveChicken), userInfo: nil, repeats: true)
+        var timerMoveChicken = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(moveChicken), userInfo: nil, repeats: true)
+        var timerCheckCollision = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(intersect), userInfo: nil, repeats: true)
         
     }
     
@@ -50,7 +54,7 @@ class PlayViewController: UIViewController {
     }
     @IBAction func pushButtonDown(_ sender: Any) {
         //move hero down
-        if (imageHero.frame.origin.y < 30) {
+        if (imageHero.frame.origin.y < 450) {
             UIView.animate(withDuration: 0.5, animations: {
                 var frameTemp = self.imageHero.frame
                 frameTemp.origin.y = frameTemp.origin.y + CGFloat(self.motionDistance)
@@ -81,40 +85,59 @@ class PlayViewController: UIViewController {
     }
     
     func moveChicken(){
-        UIView.animate(withDuration: 5, animations: {
-            //var frameTemp = self.imageChiecken1.frame
-            //frameTemp.origin.x = frameTemp.origin.x + 50
-            //frameTemp.origin.y = frameTemp.origin.y + 50
-            
-            
-            let circlePath = UIBezierPath(arcCenter: CGPoint(x:150, y:300), radius: 150, startAngle: 0, endAngle: 130, clockwise: true)
-            
-            let animation = CAKeyframeAnimation(keyPath: "position");
-            animation.duration = 40
-            animation.repeatCount = MAXFLOAT
-            animation.path = circlePath.cgPath
-            self.imageChiecken1.layer.add(animation, forKey: nil)
-            //self.imageChiecken1.frame = frameTemp
-            
-
+        if (imageChiecken1.frame.origin.y < 450 && !(imageChiecken1.frame.origin.y > 450) && !isUpward) {
+            yChickenMotion = 40
+        }
+        else if (imageChiecken1.frame.origin.y > 450){
+            yChickenMotion = -40
+            isUpward = true
+        }
+        else if (imageChiecken1.frame.origin.y < 50) {
+            isUpward = false
+        }
+        
+ /*       if (imageChiecken1.frame.origin.x < 200 && !(imageChiecken1.frame.origin.y > 200) && !isUpward) {
+            xChickenMotion = 40
+        }
+        else if (imageChiecken1.frame.origin.y > 450){
+            yChickenMotion = -40
+            isUpward = true
+        }
+        else if (imageChiecken1.frame.origin.y < 50) {
+            isUpward = false
+        }
+   */     
+        UIView.animate(withDuration: 0.5, animations: {
+            var frameTemp = self.imageChiecken1.frame
+            frameTemp.origin.x = frameTemp.origin.x + CGFloat(self.xChickenMotion)
+            frameTemp.origin.y = frameTemp.origin.y + CGFloat(self.yChickenMotion)
+            print(self.yChickenMotion)
+            self.imageChiecken1.frame = frameTemp
         })
+        
+    }
     
+    func intersect(){
+        //print(imageHero.layer.frame.origin.x)
+        if(imageChiecken1.layer.frame.intersects(imageHero.layer.frame)){
+            print("Collide...!")
+        }
     }
     
     func moveChickenInCurveMotion() {
         /*let path = UIBezierPath()
-        //path.addArc(withCenter: CGPoint(x: 100, y:100), radius: 30, startAngle: 30, endAngle: 140, clockwise: true)
-        path.move(to: CGPoint(x: 16,y: 239))
-        path.addCurve(to: CGPoint(x: 301, y: 239), controlPoint1: CGPoint(x: 136, y: 373), controlPoint2: CGPoint(x: 178, y: 110))
-
-        let animate = CAKeyframeAnimation(keyPath: "position")
-        animate.path = path.cgPath
-        animate.rotationMode = kCAAnimationRotateAuto
-        animate.repeatCount = Float.infinity
-        animate.duration = 5.0
-        imageChiecken1.layer.add(animate, forKey: "Moving")
-        
-        */
+         //path.addArc(withCenter: CGPoint(x: 100, y:100), radius: 30, startAngle: 30, endAngle: 140, clockwise: true)
+         path.move(to: CGPoint(x: 16,y: 239))
+         path.addCurve(to: CGPoint(x: 301, y: 239), controlPoint1: CGPoint(x: 136, y: 373), controlPoint2: CGPoint(x: 178, y: 110))
+         
+         let animate = CAKeyframeAnimation(keyPath: "position")
+         animate.path = path.cgPath
+         animate.rotationMode = kCAAnimationRotateAuto
+         animate.repeatCount = Float.infinity
+         animate.duration = 5.0
+         imageChiecken1.layer.add(animate, forKey: "Moving")
+         
+         */
         
         
         //let circlePath = UIBezierPath(arcCenter: CGPoint(view.frame.midX, view.frame.midY), radius: 20, startAngle: 0, endAngle:CGFloat(M_PI)*2, clockwise: true)
